27a6a6d680ba02e6b0c38889e29f0aba22253fb3
tifufu <dematasjoshua@hotmail.com>
2024-01-27 02:09:58 +0100

    charm huh select form to select backup file in restore command. Have not implemented the actual restore yet

--- cmd/backups/restore.go
+++ cmd/backups/restore.go
@@ -2,9 +2,12 @@ package backups
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/charmbracelet/huh"
 	"github.com/spf13/cobra"
 	"github.com/tifye/hosts-file-editor-cli/cmd/cli"
+	"github.com/tifye/hosts-file-editor-cli/pkg"
 )
 
 func newRestoreCommand(hostsCli cli.Cli) *cobra.Command {
@@ -13,7 +16,29 @@ func newRestoreCommand(hostsCli cli.Cli) *cobra.Command {
 		Short: "Restore from a backup",
 		Long:  ``,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("backups/restore called")
+			backups, err := pkg.GetListOfBackups()
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			var selectOpts []huh.Option[*pkg.Backup]
+			for _, backup := range backups {
+				key := fmt.Sprintf("%s %s", backup.Time, backup.Comment)
+				selectOpts = append(selectOpts, huh.NewOption(key, &backup))
+			}
+
+			var selected *pkg.Backup
+			huh.NewSelect[*pkg.Backup]().
+				Title("Select a backup").
+				Options(selectOpts...).
+				Value(&selected).
+				Run()
+
+			if selected == nil {
+				return
+			}
+
+			fmt.Println(selected.Filepath)
 		},
 	}
 
